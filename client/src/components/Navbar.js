907aa5775bef782211b79e002a852813197d45a4
Aravind pj <aravindjayan1245@gmail.com>
2023-03-02 11:11:36 +0530

    Added avatar profile

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -8,6 +8,7 @@ import {Link, useNavigate} from 'react-router-dom'
 import Cookies from 'js-cookie'
 import { useDispatch, useSelector } from 'react-redux';
 import {logout} from '../store/auth'
+import Avatar from '@mui/material/Avatar';
 export default function Navbar() {
   const isAuthenticated=useSelector(state=>state.auth.isAuthenticated)
   const dispatch=useDispatch()
@@ -19,19 +20,26 @@ export default function Navbar() {
   }
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" color='primary'>
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             <Link to="/" className='Nav-buttons'> Expensor</Link>
           </Typography>
-          <Link to='/category' className='Nav-buttons'><Button color="inherit" >Category</Button></Link>
-          {isAuthenticated && <Button onClick={_logout}  color="inherit" >Logout</Button>}
+          
+          {isAuthenticated &&
+          <>
+            <Link to='/category' className='Nav-buttons'><Button color="inherit" >Category</Button></Link>
+           <Button onClick={_logout}  color="inherit" >Logout</Button>
+           <Avatar sx={{marginLeft:3}} alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+          </> 
+          }
           {
              !isAuthenticated 
              &&
               <>
                 <Link to='/login' className='Nav-buttons'><Button color="inherit" >Login</Button></Link>
                 <Link to='/register' className='Nav-buttons'><Button color="inherit" >Register</Button></Link>
+                
               </>
           }
 
